ae8fc4591d99570c29f253d8e22af5020fec9758
niiima <nima.bastani@gmail.com>
2020-12-12 23:42:38 +0330

    Update(TypeScript): To v4.0.0beta

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,7 +4,7 @@ const About = () => (
     <div>
       <h1>Covid-19 VS Air Quality</h1>
       <p>Experimenting next.js ecosystem using Covid-19 data</p>
-      <p>Using <b>TypeScript</b>
+      <p>Written in <b>TypeScript</b>
         to know better about <i>React</i> and to take over the data flow with type safety.</p>
     </div>
   </Layout>
