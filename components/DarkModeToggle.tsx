0ded73a8fabd95290bf3e3ebad81d3f29bef37c8
= <=>
2020-12-23 12:17:05 +0330

    Fix(missing &): On darkMode toggler's svg

--- components/DarkModeToggle.tsx
+++ components/DarkModeToggle.tsx
@@ -9,7 +9,6 @@ const DarkModeToggle = () => {
                 <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" />
                 </svg>
             <input type="checkbox" checked={darkMode.value} onChange={darkMode.toggle} hidden />
-
         </ToggleButton>
     );
 };
@@ -19,7 +18,7 @@ const ToggleButton = styled.span`
  height: 30px;
  flex-shrink: 0;
  border:none;
- svg {
+ & svg {
         stroke:silver !important;
         stroke-width:1.4px; 
         width: 30px;
