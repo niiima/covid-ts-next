ec551eed321df2bf1363d07934e12b4cde9b3602
= <=>
2020-12-27 02:33:52 +0330

    Add(Title): As a build component for summary info

--- components/CovidSummary.tsx
+++ components/CovidSummary.tsx
@@ -1,101 +1,63 @@
-import { HiOutlineUserRemove } from 'react-icons/hi';
-import styled from 'styled-components';
+import { RiPulseLine, RiVirusLine, RiStethoscopeLine } from 'react-icons/ri'
+
+const Title = (props) => {
+    return (
+        <div className="col-xl-3 col-md-3 mb-4">
+            <div className="card border-left-info shadow h-100 py-2">
+                <div className="card-body" style={{ padding: "1em" }}>
+                    <div className="row no-gutters align-items-center">
+                        <div className="col mr-2">
+                            <div className="text-xs font-weight-bold text-uppercase mb-1" style={{ color: props.color}}>{props.name} {props.icon}</div>
+                            <div className="row no-gutters align-items-center">
+                                <div className="col-auto">
+                                    <div className="h5 mb-0 mr-3 font-weight-bold"
+                                        style={{ letterSpacing: "0.2em", color: props.color }}>{props.value}
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-auto">
+                                <i className="fa fa-users fa-2x text-info"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
 const CovidSummary = (props) => {
-    const { TotalConfirmed,
-        TotalDeaths,
-        TotalRecovered } = props.summaryInfo;
+    const titles = [{
+        name: "TotalConfirmed",
+        value: props.summaryInfo.TotalConfirmed,
+        icon: <RiVirusLine></RiVirusLine>,
+        color: "none"
+    }, {
+        name: "TotalDeaths",
+        value: props.summaryInfo.TotalDeaths,
+        icon: <RiPulseLine></RiPulseLine>,
+        color: "red"
+    }, {
+        name: "TotalRecovered",
+        value: props.summaryInfo.TotalRecovered,
+        icon: <RiStethoscopeLine></RiStethoscopeLine>,
+        color: "green"
+    }];
     return (
         <div className="container-fluid">
             <div className="row">
-                <div className="col-xl-3 col-md-3 mb-4">
-                    <div className="card border-left-info shadow h-100 py-2">
-                        <div className="card-body">
-                            <div className="row no-gutters align-items-center">
-                                <div className="col mr-2">
-                                    <div className="text-xs font-weight-bold text-info text-uppercase mb-1">Total Cases</div>
-                                    <div className="row no-gutters align-items-center">
-                                        <div className="col-auto">
-                                            <div className="h5 mb-0 mr-3 font-weight-bold ">{TotalConfirmed}</div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                                <div className="col-auto">
-                                    <i className="fa fa-users fa-2x text-info"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-xl-3 col-md-3 mb-4">
-                    <div className="card border-left-danger shadow h-100 py-2">
-                        <div className="card-body">
-                            <div className="row no-gutters align-items-center">
-                                <div className="col mr-2">
-                                    <div className="text-xs font-weight-bold text-danger text-uppercase mb-1">Total Death</div>
-                                    <div className="row no-gutters align-items-center">
-                                        <div className="col-auto">
-                                            <div className="h5 mb-0 mr-3 font-weight-bold ">{TotalDeaths}
-                                            <HiOutlineUserRemove  color={"red"}></HiOutlineUserRemove></div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                                <div className="col-auto">
-                                    <i className="fa fa-bed fa-2x text-danger"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-xl-3 col-md-3 mb-4">
-                    <div className="card border-left-success shadow h-100 py-2">
-                        <div className="card-body">
-                            <div className="row no-gutters align-items-center">
-                                <div className="col mr-2">
-                                    <div className="text-xs font-weight-bold text-success text-uppercase mb-1">Total Recovered</div>
-                                    <div className="row no-gutters align-items-center">
-                                        <div className="col-auto">
-                                            <div className="h5 mb-0 mr-3 font-weight-bold ">{TotalRecovered}</div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                                <div className="col-auto">
-                                    <i className="fa fa-child fa-2x text-success"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* <div className="col-xl-3 col-md-3 mb-4">
-                   <div className="card border-left-warning shadow h-100 py-2">
-                                                       <div className="card-body">
-                           <div className="row no-gutters align-items-center">
-                               <div className="col mr-2">
-                                   <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">New Cases</div>
-                                   <div className="row no-gutters align-items-center">
-                                   <div className="col-auto">
-                                           <div className="h5 mb-0 mr-3 font-weight-bold ">${new_cases}</div>
-                                       </div>
-                                                                                       
-                                   </div>
-                               </div>
-                               <div className="col-auto">
-                                   <i className="fa fa-bell fa-2x text-warning"></i>
-                               </div>
-                           </div>
-                       </div>
-                   </div>
-               </div> */}
+                {
+                    titles.map(item => (<Title
+                        name={item.name}
+                        value={item.value}
+                        key={item.name}
+                        icon={item.icon}
+                        color={item.color}>
+                    </Title>))
+                }
             </div>
         </div>
     );
 }
 
-const SyledSummary = styled(CovidSummary)`
-& .card>*{
-    background-color:white !important;
-}
-`
-export default SyledSummary;
+export default CovidSummary;
